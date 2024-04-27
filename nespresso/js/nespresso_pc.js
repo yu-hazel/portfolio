a10018cdd201a31144607e0120cee36a77fd1ac4
hyein <yoohi960201@gmail.com>
2024-04-27 19:15:53 +0900

    0426_네스프레소

--- nespresso/js/nespresso_pc.js
+++ nespresso/js/nespresso_pc.js
@@ -31,4 +31,36 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 });
 
+var swiper = new Swiper(".collabo", {
+    slidesPerView: 2.5,
+    spaceBetween: 50,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        // 640: {
+        //     slidesPerView: 2,
+        //     spaceBetween: 20,
+        // },
+        // 768: {
+        //     slidesPerView: 4,
+        //     spaceBetween: 40,
+        // },
+        // 1024: {
+        //     slidesPerView: 5,
+        //     spaceBetween: 50,
+        // },
+    },
+});
 
+// 페이지 최상단으로 스크롤 이동하는 함수
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // 부드러운 스크롤
+    });
+}
+
+// 페이지 최상단으로 이동하는 버튼에 클릭 이벤트를 추가
+document.querySelector('.pageTop').addEventListener('click', scrollToTop);
