5a1b3087b53fb1d5f6cd3af31410fdbb095d02f0
hyein <yoohi960201@gmail.com>
2024-05-16 21:49:43 +0900

    단양관광 캠핑장 섹션 작업

--- tour/js/danyang_tour.js
+++ tour/js/danyang_tour.js
@@ -10,3 +10,20 @@ document.getElementById('lang').addEventListener('click', function() {
         }
     }
 })
+
+var swiper = new Swiper(".camping", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
