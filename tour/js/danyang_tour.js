d4ec00be10759cfb5b1687e481185a4286c579cd
hyein <yoohi960201@gmail.com>
2024-06-05 16:42:38 +0900

    한국관광공사 api 자료 수집

--- tour/js/danyang_tour.js
+++ tour/js/danyang_tour.js
@@ -135,3 +135,50 @@ snsBt.forEach((eachBt, index) => {
     })
 })
 snsBt[0].click();
+
+// 충북관광 API
+
+const proxyURL = 'https://api.allorigins.win/get?url=';
+        const apiURL = encodeURIComponent('https://tour.chungbuk.go.kr/openapi/tourInfo/attr.do?searchCnd=tourNm&searchKrwd=%EB%8B%A8%EC%96%91&PageUnit=15');
+
+        async function fetchData() {
+            try {
+                const response = await fetch(proxyURL + apiURL);
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+
+                const data = await response.json();
+
+                const actualData = JSON.parse(data.contents);
+                // console.log(actualData);
+
+                const tourNames = actualData.result.map(item => item.tourNm);
+                // ✅ map 메서드를 이용해서 배열에서 각 요소의 특정 값을 모두 가져오게 할 수 있다.
+                // map 메서드의 item은 actualData.result 배열의 각 요소를 뜻한다.
+                // 배열의 각 요소를 순회하며, 그 요소를 함수의 인자로 전달한다.
+
+                console.log(tourNames);
+                // content.textContent = tourNames;
+                // content.innerContent = actualData.
+                // document.getElementById('content').innerText = JSON.stringify(actualData, null, 2);
+                // console.log(actualData.result[0].thumbImg);
+
+                // const imageUrl = actualData.result[7].thumbImg;
+                // document.getElementById('image').src = imageUrl;
+
+                // const imageUrl2 = actualData.result[3].thumbImg;
+                // document.getElementById('image2').src = imageUrl2;
+
+                // document.getElementById('content').style.backgroundImage = `url(${imageUrl})`;
+                // document.getElementById('content').style.backgroundSize = 'cover';
+
+                // const location01 = actualData.result[5].intrcn
+                // document.getElementById('location').innerText = location01;
+            } catch (error) {
+                console.error('Fetch operation failed: ', error);
+            }
+        }
+
+        fetchData();
